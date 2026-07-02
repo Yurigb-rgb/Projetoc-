fa8ff65f91ea98dba827d9bdf7b20d9a01ad5739
Yurigb-rgb <ygsr98@gmail.com>
2026-07-01 23:05:18 -0300

    :tada: Commit Inicial

---
+++ Atividade2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel;
+using System.Net.Http.Headers;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Channels;
+
+class Menuselecao
+{
+    public static void Main()
+    {
+
+        int opcao = 1;
+        while (opcao != 3)
+        {
+
+            
+            Console.WriteLine($"Escolha uma opcao 1- cadastrar");
+        Console.WriteLine($"Escolha uma opcao 2- Listar");
+        Console.WriteLine($"Escolha uma opcao 3- Sair ");
+
+        
+            string escolha = Console.ReadLine();
+        int.TryParse(escolha, out opcao );
+        
+
+        
+                switch (opcao)
+                {
+                    case 1:
+                        Console.WriteLine("Cadastrar aluno: ");
+                        break;
+                    case 2:
+                        Console.WriteLine("Listar alunos: ");
+                        break;
+                    case 3:
+                        Console.WriteLine("Encerrando programa");
+                        break;
+
+                }
+            
+        }
+    }
+
+}
+
+    
+
+
+
+
+
+
+
+        
+
+
