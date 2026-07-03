87d5502201b529c8c5050acb5696173910033898
Yurigb-rgb <ygsr98@gmail.com>
2026-07-03 10:37:59 -0300

    Atualizando projeto

--- Atividade2/Program.cs
+++ Atividade2/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System.ComponentModel;
 using System.Net.Http.Headers;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Channels;
+
+
+class Aluno
+{
+    public string Nome;
+    public double Media;
+}
+
 
 class Menuselecao
 {
     public static void Main()
     {
+      List<Aluno> alunos = new List<Aluno>();
+        
+        
 
         int opcao = 1;
         while (opcao != 3)
@@ -20,15 +32,25 @@ class Menuselecao
         
             string escolha = Console.ReadLine();
         int.TryParse(escolha, out opcao );
-        
 
-        
-                switch (opcao)
-                {
-                    case 1:
-                        Console.WriteLine("Cadastrar aluno: ");
-                        break;
-                    case 2:
+            switch 
+                (opcao)
+            {
+
+                case 1:
+                    Console.WriteLine("Cadastrar aluno: ");
+
+                    string Nome = Console.ReadLine();
+                    Console.WriteLine($"Media de {Nome}:");
+                    string aluno = Console.ReadLine();
+
+                    double.TryParse(aluno, out double Media);
+                    var aluno1 = new Aluno();
+                    aluno1.Nome = Nome;
+                    aluno1.Media = Media;
+
+                    break;
+                case 2:
                         Console.WriteLine("Listar alunos: ");
                         break;
                     case 3:
